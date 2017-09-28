629de404d94d281ca659fbba0e63b543cb60fd62
monikasmilga <tuuli705@gmail.com>
2017-09-28 11:00:34 +0300

    Added middleware

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'user', 'middleware' => ['auth', 'user-check']], function () {
+
+    Route::get('/posts', ['as' => 'app.posts.create', 'uses' => 'DBPostsController@create']);
+
+    Route::post('/posts', ['as' => 'app.post.store', 'uses' => 'DBPostsController@store']);
+
+});
