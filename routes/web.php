c402579dfaca6700520c3c7413ff6ed5611a5943
monikasmilga <tuuli705@gmail.com>
2017-09-28 16:01:55 +0300

    To be continued

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', ['as' => 'app.home.index', 'uses' => 'HomeController@index']);
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', ['as' => 'app.home.index', 'uses' => 'HomeController@index']);
+
 
 Route::group(['prefix' => 'user', 'middleware' => ['auth', 'user-check']], function () {
 
-    Route::get('/posts', ['as' => 'app.posts.create', 'uses' => 'DBPostsController@create']);
+    Route::get('/add-post', ['as' => 'app.post.create', 'uses' => 'EBPostsController@create']);
 
-    Route::post('/posts', ['as' => 'app.post.store', 'uses' => 'DBPostsController@store']);
+    Route::post('/add-post', ['as' => 'app.post.store', 'uses' => 'EBPostsController@store']);
 
+    Route::get('/posts', ['as' => 'app.posts.index', 'uses' => 'EBPostsController@index']);
+
+    Route::group(['prefix' => '{id}'], function () {
+
+        Route::get('/', ['as' => 'app.posts.show', 'uses' => 'EBPostsController@show']);
+
+        Route::get('/edit', ['as' => 'app.posts.edit', 'uses' => 'EBPostsController@edit']);
+
+        Route::post('/edit', ['as' => 'app.posts.update', 'uses' => 'EBPostsController@update']);
+
+        Route::get('/delete', ['as' => 'app.posts.destroy', 'uses' => 'EBPostsController@destroy']);
+
+    });
 });
